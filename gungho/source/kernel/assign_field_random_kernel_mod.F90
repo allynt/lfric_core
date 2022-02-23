a4be39c288e7f0dbbef6903940f120b4fc061001
stevemullerworth <enquiries@metoffice.gov.uk>
2022-02-23 13:30:34 +0000

    ticket:2959 for James Cunningham-Smith: Random field kernel should take a scalar multiplier
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@34147 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- gungho/source/kernel/assign_field_random_kernel_mod.F90
+++ gungho/source/kernel/assign_field_random_kernel_mod.F90
@@ -7,14 +7,15 @@
 !-------------------------------------------------------------------------------
 !> @brief Assign random values to a field
 !> @details Sets all field values to uniformly distributed random numbers
-!> in the range [0,1].
+!> in the range [0,scale] for some specified scale.
 
 module assign_field_random_kernel_mod
 
 use argument_mod,            only : arg_type,            &
                                     GH_FIELD, GH_REAL,   &
                                     GH_INC, ANY_SPACE_1, &
-                                    CELL_COLUMN
+                                    CELL_COLUMN, GH_READ, &
+                                    GH_SCALAR
 use constants_mod,           only : r_def, i_def
 use kernel_mod,              only : kernel_type
 
@@ -28,9 +29,10 @@ private
 
 type, public, extends(kernel_type) :: assign_field_random_kernel_type
   private
-  type(arg_type) :: meta_args(1) = (/                   &
-       arg_type(GH_FIELD, GH_REAL, GH_INC, ANY_SPACE_1) &
-       /)
+  type(arg_type) :: meta_args(2) = (/                  &
+    arg_type(GH_FIELD,  GH_REAL, GH_INC, ANY_SPACE_1), &
+    arg_type(GH_SCALAR, GH_REAL, GH_READ )             &
+  /)
   integer :: operates_on = CELL_COLUMN
 contains
   procedure, nopass :: assign_field_random_code
@@ -45,11 +47,13 @@ contains
 !> @brief Sets all field entries to random values
 !> @param[in] nlayers Number of layers
 !> @param[in,out] x Output data
+!> @param[in] scale Output values are in range [0, scale]
 !> @param[in] ndf Number of degrees of freedom per cell for the output field
 !> @param[in] undf Unique number of degrees of freedom  for the output field
 !> @param[in] map Dofmap for the cell at the base of the column for the output field
-subroutine assign_field_random_code(nlayers,     &
-                                    x,           &
+subroutine assign_field_random_code(nlayers,       &
+                                    x,             &
+                                    scale,         &
                                     ndf, undf, map)
   implicit none
 
@@ -58,6 +62,7 @@ subroutine assign_field_random_code(nlayers,     &
   integer(kind=i_def),                   intent(in)    :: undf, ndf
   integer(kind=i_def), dimension(ndf),   intent(in)    :: map
   real   (kind=r_def), dimension(undf),  intent(inout) :: x
+  real   (kind=r_def),                   intent(in)    :: scale
 
   ! Internal variables
   integer(kind=i_def)              :: df, k
@@ -66,7 +71,7 @@ subroutine assign_field_random_code(nlayers,     &
   do k = 0, nlayers-1
     call random_number(random_values(:))
     do df = 1, ndf
-      x(map(df)+k) = random_values(df)
+      x(map(df)+k) = random_values(df) * scale
     end do
   end do
 
