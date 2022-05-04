6b38065841f69de07a855241b6aa6a1a49ce7ea9
ivakavcic <enquiries@metoffice.gov.uk>
2022-05-04 14:52:13 +0000

    ticket:3246 for Andy Coughtrie: Fix size(chain_mesh_tags) bug in driver_mesh_mod
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@35529 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- components/driver/source/driver_mesh_mod.f90
+++ components/driver/source/driver_mesh_mod.f90
@@ -177,8 +177,8 @@ subroutine init_mesh( local_rank, total_ranks,        &
 
   if ( present(use_multigrid) ) then
     create_multigrid = use_multigrid
-    n_chain_meshes = size(chain_mesh_tags)
     if ( create_multigrid ) then
+      n_chain_meshes = size(chain_mesh_tags)    
       if ( present(multigrid_mesh_ids) ) then
         create_multigrid_meshes    = .true.
         if (allocated(multigrid_mesh_ids)) deallocate(multigrid_mesh_ids)
