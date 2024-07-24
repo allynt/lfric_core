2b3bb79e4474a4070ec107ba020881ad962e9eff
lucygordon <enquiries@metoffice.gov.uk>
2024-07-24 10:07:09 +0000

    ticket:4405 for mikehobson: Templating operators
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@50743 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- infrastructure/build/import.mk
+++ infrastructure/build/import.mk
@@ -14,7 +14,9 @@ TYPE_TABLE_int32 = integer
 .PHONY: import-infrastructure
 import-infrastructure: $(WORKING_DIR)/field/field_real32_mod.f90 \
                        $(WORKING_DIR)/field/field_real64_mod.f90 \
-		       $(WORKING_DIR)/field/field_int32_mod.f90
+                       $(WORKING_DIR)/field/field_int32_mod.f90 \
+                       $(WORKING_DIR)/operator/operator_real32_mod.f90 \
+                       $(WORKING_DIR)/operator/operator_real64_mod.f90
 	$Q$(MAKE) $(QUIET_ARG) -f $(LFRIC_BUILD)/extract.mk SOURCE_DIR=$(LFRIC_INFRASTRUCTURE)/source
 	$Q$(MAKE) $(QUIET_ARG) -f $(LFRIC_BUILD)/psyclone/psyclone.mk \
 	          SOURCE_DIR=$(LFRIC_INFRASTRUCTURE)/source \
@@ -26,4 +28,12 @@ $(WORKING_DIR)/field/field_%_mod.f90: $(LFRIC_INFRASTRUCTURE)/source/field/field
 	$Q$(TEMPLATE_TOOL) $< -o $@ -s type=$(TYPE_TABLE_$*) -s kind=$*
 
 $(WORKING_DIR)/field:
+	$Qmkdir -p $@
+
+$(WORKING_DIR)/operator/operator_%_mod.f90: $(LFRIC_INFRASTRUCTURE)/source/operator/operator_mod.t90 \
+                                      | $(WORKING_DIR)/operator
+	$(call MESSAGE, Templating, $<)
+	$Q$(TEMPLATE_TOOL) $< -o $@ -s kind=$*
+
+$(WORKING_DIR)/operator:
 	$Qmkdir -p $@
